ea3af10c739835d031c1546d3c6262d047f08f93
Nicolas Schmitt <79269969+schmittnicolas@users.noreply.github.com>
2022-03-08 20:14:47 +0100

    Add files via upload

---
+++ cosmic-conquistadors/src/DefaultCritter.java
@@ -0,0 +1,34 @@
+public class DefaultCritter {
+
+    // Position
+    private int X;
+    private int Y;
+
+    // Constructor
+    public DefaultCritter(){
+        X = 0;
+        Y = 0;
+    }
+    public DefaultCritter(int x, int y){
+        X = x;
+        Y = y;
+    }
+
+    // Get
+    public int getX(){
+        return X;
+    }
+
+    public int getY(){
+        return Y;
+    }
+
+    // Set
+    public void setX(int x){
+        X = x;
+    }
+
+    public void setY(int y){
+        Y = y;
+    }
+}
