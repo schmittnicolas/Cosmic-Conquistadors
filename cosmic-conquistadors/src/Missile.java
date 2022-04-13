f3015843ca491e9da52aba4e476b3a88dcd8bf8d
BjornvDyk1 <101084136+BjornvDyk1@users.noreply.github.com>
2022-04-13 08:14:28 +0200

    Update missile class
    Add angle functionality

--- cosmic-conquistadors/src/Missile.java
+++ cosmic-conquistadors/src/Missile.java
@@ -1,10 +1,22 @@
 public class Missile extends DefaultCritter{
 
-    public Missile(double x, double y){
-        super(x, y, 1.5, 5, 0,  "images/missile.png");
+    public Missile(double x, double y ,double angle){
+        super(x, y, 1.5, 5, angle,  "images/missile.png");
     }
 
-    public boolean Move(){
+    // function to control movement of missile
+    public boolean Move() {
+        double myDegrees = this.Degrees / 180 * Math.PI; // convert to radians
+        double newY = this.Y + (this.Speed*Math.sin(myDegrees + (Math.PI/2))); // calculate next y value
+        double newX = this.X + (this.Speed*Math.cos(myDegrees + (Math.PI/2))); // calculate next x value
+        if (newX < InvaderGameState.scale && newY < InvaderGameState.scale && newX > 0 && newY > 0) {
+            this.Y = newY;
+            this.X = newX;
+            return true;
+        }
+        return false;
+    
+    /*public boolean Move(){
         double newY = this.Y + this.Speed;
         if (newY <= InvaderGameState.scale){
             this.Y = newY;
@@ -12,5 +24,5 @@ public class Missile extends DefaultCritter{
         }
 
         return false;
-    }
+    }*/
 }
